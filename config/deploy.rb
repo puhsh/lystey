2cb2b164c6a818a7ebb0d2322cf1f3c085e00917
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-04-09 11:53:49 -0500

    Actually use the .ruby-version and gemsite files

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5
 set :rvm_type, :auto
+set :rvm_ruby_version, 'rvm current'
 
 namespace :deploy do
 end
