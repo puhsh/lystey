42f6917dbda1dcef6945d224ebc985dc285d9d63
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-06-09 23:02:24 -0500

    Initial form

--- app/assets/javascripts/components/admin.jsx
+++ app/assets/javascripts/components/admin.jsx
@@ -32,6 +32,18 @@ var AdminContent = React.createClass({
       <div className="row">
         <div className="twelve columns">
           <h1 className="section-heading">{this.props.sectionName}</h1>
+          <form className="form">
+            <div className="row">
+              <div className="twelve columns">
+                <input placeholder="First Name" type="text" className="u-full-width" />
+                <input placeholder="Last Name" type="text" className="u-full-width" />
+                <input placeholder="Suffix" type="text" className="u-full-width" />
+                <input placeholder="License Number" type="text" className="u-full-width" />
+                <input placeholder="License Type" type="text" className="u-full-width" />
+                <input placeholder="License Country" type="text" className="u-full-width" />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     )
