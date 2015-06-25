0dfe8db6978983de9f2526e4a67ae8f5cdc9bd7d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-06-25 11:26:00 -0500

    Build a theme for agents when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 Agent.destroy_all
-Agent.create(registered: true, first_name: "Registered", last_name: "Agent", license_number: "123456789", phone_number: "555-555-5555", email: 'test@test.local')
-Agent.create(registered: false, first_name: "Unregistred", last_name: "Agent", license_number: "987654321", phone_number: "555-555-5555", email: 'test2@test.local')
+agent = Agent.create(registered: true, first_name: "Registered", last_name: "Agent", license_number: "123456789", phone_number: "555-555-5555", email: 'test@test.local')
+agent2 = Agent.create(registered: false, first_name: "Unregistred", last_name: "Agent", license_number: "987654321", phone_number: "555-555-5555", email: 'test2@test.local')
+
+# Generate a theme
+agent.build_theme
+agent.save
+
+agent2.build_theme
+agent2.save
