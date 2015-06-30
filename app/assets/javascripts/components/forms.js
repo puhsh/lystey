dd2b83a50d32d889c0974d60d1769e64ccde3f52
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-06-30 18:43:10 -0500

    Update the color theme preview accordingly

--- app/assets/javascripts/components/forms.js
+++ app/assets/javascripts/components/forms.js
@@ -14,14 +14,48 @@ var App = window.App || {};
     $(".js-color-picker").spectrum({
       replacerClassName: 'color-preview',
       showButtons: false,
-      preferredFormat: 'hex'
-    });
-
-    $(".js-color-picker").show();
+      preferredFormat: 'hex',
+    }).show();
 
     $(".js-color-picker").on("focusout", _.debounce(function(e) {
       $(e.target).spectrum("set", $(e.target).val());
+      updateAdminThemePreviewColors($(e.target));
     }, 100));
+
+    $(".js-color-picker").on("change.spectrum", function(e) {
+      updateAdminThemePreviewColors($(e.target));
+    });
+  }
+
+  function updateAdminThemePreviewColors($target) {
+    var key, attr;
+    var cssProp = {};
+    var targetProp = $target.data("target");
+    switch (targetProp) {
+      case "admin-background":
+        key = "background-color";
+        attr = "#admin-theme-preview.js-preview-update";
+        attr
+        break;
+      case "admin-font-color":
+        key = "color";
+        attr = "#admin-theme-preview.js-preview-update h1";
+        break;
+      case "admin-button-color":
+        key = "background-color";
+        attr = "#admin-theme-preview.js-preview-update button";
+        break;
+      case "admin-button-text-color":
+        key = "color";
+        attr = "#admin-theme-preview.js-preview-update button";
+        break;
+      default:
+        return;
+    }
+
+    cssProp[key] = $target.val();
+
+    $(attr).css(cssProp);
   }
 
   $(function() {
